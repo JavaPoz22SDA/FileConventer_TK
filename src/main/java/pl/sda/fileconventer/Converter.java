7cb53cd846baf8fc8dfd421c4974fc8af8119227
Tomasz Krutowski <tkrutowski@gmail.com>
2020-05-16 10:25:56 +0200

    add json file

---
+++ src/main/java/pl/sda/fileconventer/Converter.java
@@ -0,0 +1,8 @@
+package pl.sda.fileconventer;
+
+public class Converter {
+    public void convert(String sourceFilePath,String outputFilePath){
+
+
+    }
+}
--- src/main/java/pl/sda/fileconventer/Main.java
+++ src/main/java/pl/sda/fileconventer/Main.java
@@ -3,5 +3,9 @@ package pl.sda.fileconventer;
 public class Main {
     public static void main(String[] args) {
 
+        Converter converter = new Converter();
+        String sourceFilePath = "test.json";
+        String outputFilePath = "test.xlsx";
+        converter.convert(sourceFilePath, outputFilePath);
     }
 }
